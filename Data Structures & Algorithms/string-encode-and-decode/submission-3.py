51461e3a6c324ffcdb228c5640b63beac3ea41bc
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-05-06 09:28:46 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            encoded = encoded + str(len(s)) + "-" + s
+        return encoded
+
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        num = ""
+        i = 0
+        while i < len(s):
+            word = ""
+            if(s[i].isnumeric()):
+                num += s[i]
+                i+=1
+            elif(s[i] == "-"):
+                word = s[i+1: i+1+int(num)]
+                decoded.append(word)
+                i += 1+int(num)
+                num = ""
+        return decoded
+
+
