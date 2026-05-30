2459b25aa6789ac38f35a2f2061f461290db37f2
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-05-29 17:58:48 -0700

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        sol = []
+
+        for i in range(n):
+            count = 1
+            j = i + 1
+            while j < n:
+                if temperatures[j] > temperatures[i]:
+                    break
+                j+=1
+                count+=1
+            
+            if j == n: count = 0
+            sol.append(count)
+        
+        return sol
+
+
+        
